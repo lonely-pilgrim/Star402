7870254b2b4be81386c8c65b4e80015c8db60454
Егор 🦁 <egorprog6@gmail.com>
2024-04-16 15:19:35 +0300

    Добавьте файлы проекта.

---
+++ Star.git.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+void drawTriangle(int n) {
+    for (int i = 0; i < n; ++i) {
+
+        for (int j = 0; j < n - i - 1; ++j) {
+            cout << " ";
+        }
+
+        for (int j = 0; j < 2 * i + 1; ++j) {
+            cout << "*";
+        }
+        cout << std::endl;
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int height;
+    cout << "Введите высоту треугольника: ";
+    cin >> height;
+
+    drawTriangle(height);
+
+    return 0;
+}
